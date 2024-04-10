4afc4f15ae488d9216234f915df82fd5f5a5cacc
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-04-10 13:14:42 +0600

    function added

---
+++ scripts.js/app.js
@@ -0,0 +1,21 @@
+const price = document.getElementById('budget').innerText;
+
+// console.log(price);
+const convertPrice = parseInt(price);
+
+
+const cartCount = document.getElementById('cart').innerText;
+const convertCartCount = parseInt(cartCount);
+
+
+
+const leftCount = document.getElementById('cart').innerText;
+const convertLeftCount = parseInt(leftCount);
+
+
+function getConvertedValue(id) {
+    const price = document.getElementById('id').innerText;
+    const convertPrice = parseInt(price);
+    return convertPrice;
+
+}
