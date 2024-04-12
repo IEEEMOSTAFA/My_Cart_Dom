3fd8246d384b0e98af152f41c96a785443134499
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-04-12 10:54:47 +0600

    coupon code added

--- scripts.js/app.js
+++ scripts.js/app.js
@@ -1,21 +1,90 @@
-const price = document.getElementById('budget').innerText;
+const allBtn = document.getElementsByClassName("add-btn");
+for (const btn of allBtn) {
+    btn.addEventListener("click", function (event) {
+        const name = event.target.parentNode.childNodes[1].innerText;
+        const price = event.target.parentNode.childNodes[3].childNodes[1].innerText;
+        const category = event.target.parentNode.childNodes[5].childNodes[1].innerText;
+        // console.log(name,price,category);
 
-// console.log(price);
-const convertPrice = parseInt(price);
+        const selectedContainer = document.getElementById("selected-players-container");
+        const div = document.createElement("div");
+        div.classList.add("flex", "gap-5");
+        // div.classList.add("px-2");
+        div.classList.add("px-2", "container-with-gap");
+        const p1 = document.createElement("p");
+        const p2 = document.createElement("p");
+        const p3 = document.createElement("p");
+
+        p1.innerText = name;
+        p2.innerText = price;
+        p3.innerText = category;
+
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.appendChild(p3);
+
+        selectedContainer.appendChild(div);
+
+        updateTotalCost(price);
+        updateGrandTotal();
 
 
-const cartCount = document.getElementById('cart').innerText;
-const convertCartCount = parseInt(cartCount);
-
-
-
-const leftCount = document.getElementById('cart').innerText;
-const convertLeftCount = parseInt(leftCount);
-
-
-function getConvertedValue(id) {
-    const price = document.getElementById('id').innerText;
-    const convertPrice = parseInt(price);
-    return convertPrice;
+    });
 
 }
+function updateGrandTotal(status) {
+    const totalCost = getConvertedValue("total-cost");
+    if (status == undefined) {
+                
+        document.getElementById("grand-total").innerText = totalCost;
+    }
+    else {
+        const couponCode = document.getElementById("coupon-code").value;
+        
+        if(couponCode == "love420"){ 
+            const discounted = totalCost * 0.2;
+            // console.log(discounted);
+            document.getElementById("grand-total").innerText = totalCost - discounted;
+        }
+        else{
+            alert("Please ,Enter a valid coupon code");
+        }
+    }
+
+
+}
+
+
+
+
+function updateTotalCost(price) {
+    const totalCost = getConvertedValue("total-cost");
+    const convertedPrice = parseInt(price);
+    const sum = totalCost + convertedPrice;
+    document.getElementById("total-cost").innerText = sum;
+}
+
+function getConvertedValue(id) {
+    const price = document.getElementById(id).innerText;
+    const convertPrice = parseInt(price);
+    return convertPrice;
+}
+
+
+
+
+
+
+
+function checkParameter(status){
+    // console.log(status);
+    if (status == undefined){
+        console.log("kichu dei nai");
+    }
+    else{
+        console.log("parameter ache");
+    }
+
+}
+
+checkParameter("Mostafa")
